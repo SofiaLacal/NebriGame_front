9a18b66c9739f2673a397835cb76bfa8d78ccebf
JotaMoncho <javiermrss01@gmail.com>
2026-01-21 13:27:10 +0100

    Conexion Back y Front

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 //Página para montar todos los componentes!!!!
 
+import { useEffect, useState } from "react";
+
 function App() {
+
+  const [videojuegos, setVideojuegos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4004/videojuegos")
+      .then(res => res.json())
+      .then(data => setVideojuegos(data.videojuegos))
+      .catch(err => console.error(err));
+  }, []);
 
   return (
     <>
+      {/* Prueba roñosa */}
+      <h1>NebriGame 🎮</h1>
 
+      {videojuegos.length === 0 ? (
+        <p>Cargando videojuegos...</p>
+      ) : (
+        <ul>
+          {videojuegos.map((juego) => (
+            <li key={juego.id}>
+              {juego.nombre}
+            </li>
+          ))}
+        </ul>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
