cee0a181235b7b21853894b33da3077193308406
Sofia Lacal <sofialacrodriguez@outlook.com>
2026-01-28 20:51:10 +0100

    Añadido fondo en home + responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,53 +1,54 @@
 import Header from '../../components/Header/Header';
 import './Home.css'
-import { useEffect, useState } from "react";
 import { useVideojuegos } from "../../api/useProduct";  
 import Footer from '../../components/Footer/Footer';
 
 function Home() {
 
-  const { videojuegos, loading } = useVideojuegos();
-
-/*   const [selectedCategory, setSelectedCategory] = useState('all')
-
-  const filteredProducts = selectedCategory === 'all' 
-    ? products 
-    : products.filter(p => p.category === selectedCategory)
-
-  const featuredProducts = products.slice(0, 4) */
+  const { videojuegos } = useVideojuegos();
 
   return (
     <>
+    <div className="background">
+
       <Header/>
-      <div className="home">
-        {/* Hero Section */}
-        <section className="hero">
-          <h1>NebriGame 🎮 - PRUEBA CON BDD</h1>
-        </section>
 
-        {/* Featured Products */}
-        <section className="featured-section">
-          <h2>⭐ Productos Destacados</h2>
-          
-          <div className="hero-content">
-            {videojuegos.length === 0 ? (<p>Cargando videojuegos...</p>) : 
-            (
-              <ul>
-                {videojuegos.map((juego) => (
-                  <li key={juego.id}>
-                    {juego.nombre}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
-        </section>
+        <div className="home">
+          {/* Hero Section */}
+          <section className="hero">
+            <h1>Bienvenido a NebriGame</h1>
+            <p>Tu tienda online de videojuegos, consolas y merchandising. <br/>
+              Encuentra todo lo que necesitas con un sólo click.
+              Compra ahora o reserva los últimos lanzamientos.
+            </p>
+          </section>
+        </div>
+    </div>
 
-        {/* Categories Filter */}
-        <section className="categories-section">
-          <h2>📦 Explorar por Categoría</h2>
-        </section>
-      </div>
+        <div className="home">
+          {/* Featured Products */}
+          <section className="featured-section">
+            <h2>⭐ Productos Destacados</h2>
+            
+            <div className="hero-content">
+              {videojuegos.length === 0 ? (<p>Cargando videojuegos...</p>) : 
+              (
+                <ul>
+                  {videojuegos.map((juego) => (
+                    <li key={juego.id}>
+                      {juego.nombre}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          </section>
+
+          {/* Categories Filter */}
+          <section className="categories-section">
+            <h2>📦 Explorar por Categoría</h2>
+          </section>
+        </div>
 
       <Footer />
     </>
