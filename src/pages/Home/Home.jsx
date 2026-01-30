e2e3d451f1ace4181c0c1ae8240c5d254b94ffd3
Sofia Lacal <sofialacrodriguez@outlook.com>
2026-01-30 20:39:14 +0100

    ????

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,54 +10,53 @@ function Home() {
 
   return (
     <>
-    <div className="background">
-      <Header/>
-        <div className="home">
-          {/* Hero Section */}
-          <section className="hero">
-            <h1>Bienvenido a NebriGame</h1>
-            <p>Tu tienda online de videojuegos, consolas y merchandising. <br/>
-              Encuentra todo lo que necesitas con un sólo click.
-              Compra ahora o reserva los últimos lanzamientos.
-            </p>
-          </section>
-        </div>
-    </div>
+      <div className="background">
+        <Header/>
+          <div className="home">
+            {/* Hero Section */}
+            <section className="hero">
+              <h1>Bienvenido a NebriGame</h1>
+              <p>Tu tienda online de videojuegos, consolas y merchandising. <br/>
+                Encuentra todo lo que necesitas con un sólo click.
+                Compra ahora o reserva los últimos lanzamientos.
+              </p>
+            </section>
+          </div>
+      </div>
 
-        <div className="home">
-          {/* Featured Products */}
-          <section className="featured-section">
-            <h2>⭐ Productos Destacados</h2>
-            
-            <div className="hero-content">
-              {videojuegos.length === 0 ? (<p>Cargando videojuegos...</p>) : 
-              (
-                <ul>
-                  {videojuegos.map((juego) => (
-                    <li key={juego.id}>
-                      {juego.nombre}
-                      <img src={getImageUrl(juego.imagen_url)} alt={juego.nombre} />
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </div>
-          </section>
+      <div className="home">
+        {/* Featured Products */}
+        <section className="featured-section">
+          <h2>⭐ Productos Destacados</h2>
+          
+          <div className="hero-content">
+            {videojuegos.length === 0 ? (<p>Cargando videojuegos...</p>) : 
+            (
+              <ul>
+                {videojuegos.map((juego) => (
+                  <li key={juego.id}>
+                    {juego.nombre}
+                    <img src={getImageUrl(juego.imagen_url)} alt={juego.nombre} />
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </section>
 
-          {/* Categories Filter */}
-          <section className="categories-section">
-            <h2>📦 Explorar por Categoría</h2>
-          </section>
-        </div>
-      </section>
+        {/* Categories Filter */}
+        <section className="categories-section">
+          <h2>📦 Explorar por Categoría</h2>
+        </section>
+      </div>
 
       {/* Categories Filter */}
       <section className="categories-section">
         <h2>📦 Explorar por Categoría</h2>
       </section>
-    </div>
+
     <Footer/>
-</>
+  </>
   )
 }
 
