4e4789e3c7d069f932eb8a5e46b98a4c149cc9b2
Pablo Herrero <pablohgb@gmail.com>
2026-02-09 17:07:59 +0100

    arreglado bug carrito en productDetail

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -9,7 +9,7 @@ import Footer from "../../components/Footer/Footer";
 import getImageUrl from "../../utils/getImage";
 import useUserStore from "../../stores/userStore";
 import { useIsInWishlist, useAddWishlist, useDeleteWishlist } from "../../api/useWishlist";
-import { useIsInCart, useAddCart, useDeleteCart } from "../../api/useCart";
+import { useAddCart } from "../../api/useCart";
 
 function ProductDetail() {
   const { id, tipo } = useParams();
@@ -17,16 +17,11 @@ function ProductDetail() {
   const { isInWishlist, loading: loadingWishlist } = useIsInWishlist(userId, id);
   const [isUpdating, setIsUpdating] = useState(false);
   const [localIsInWishlist, setLocalIsInWishlist] = useState(isInWishlist);
-  const { isInCart, loading: loadingCart } = useIsInCart(userId, id);
-  const [localIsInCart, setLocalIsInCart] = useState(isInCart);
-
+ 
   // Sincronizar el estado local con el hook cuando cambie
   useEffect(() => {
     setLocalIsInWishlist(isInWishlist);
   }, [isInWishlist]);
-  useEffect(() => {
-    setLocalIsInCart(isInCart);
-  }, [isInCart]);
 
   const handleToggleWishlist = async (productId) => {
     if (isUpdating) return; // Evitar múltiples clicks
@@ -121,7 +116,7 @@ function ProductDetail() {
                   color={localIsInWishlist ? "#e74c3c" : "white"} 
                 />
               </button>
-              <button className="boton-carrito" onClick={() => handleAddToCart(producto.id)} disabled={isUpdating || loadingCart}>Añadir al carrito</button>
+              <button className="boton-carrito" onClick={() => handleAddToCart(producto.id)} disabled={isUpdating}>Añadir al carrito</button>
             </div>
           </div>
         </div>
