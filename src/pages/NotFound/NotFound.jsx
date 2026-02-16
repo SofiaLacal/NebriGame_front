b9910c3e311d1ddd5d43e2770892f46fc13a133e
Sofia Lacal <sofialacrodriguez@outlook.com>
2026-02-16 20:08:12 +0100

    Spoiler, no eran los últimos retoques

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -14,14 +14,14 @@ function NotFound({ code = 404, title = "Page Not Found", message }) {
         <button onClick={() => navigate("/")}>Home</button>
 
         {/* Sección de pruebas de errores */}
-        <div>
-          <h3>Testing de errores:</h3>
-          <div className="test-buttons">
-            <button onClick={() => navigate('/error-403')}>Test Error 403</button>
-            <button onClick={() => navigate('/error-500')}>Test Error 500</button>
-            <button onClick={() => navigate('/pagina-que-no-existe')}>Test Error 404</button>
-          </div>
-        </div>
+        {/*<div>
+            <h3>Testing de errores:</h3>
+            <div className="test-buttons">
+              <button onClick={() => navigate('/error-403')}>Test Error 403</button>
+              <button onClick={() => navigate('/error-500')}>Test Error 500</button>
+              <button onClick={() => navigate('/pagina-que-no-existe')}>Test Error 404</button>
+            </div>
+          </div> */}
         </div>
       </div>
   );
